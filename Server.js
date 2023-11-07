63583e50e9c2ba95884a71ea863f2aa50624c867
cd-JL <jmasterl@live.com>
2023-11-06 22:01:26 -0700

    Fixed all functionality, added input validation.

--- Server.js
+++ Server.js
@@ -4,16 +4,25 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve your static files (HTML, CSS, and scripts) from the same directory as the server script
 app.use(express.static(__dirname));
-
 app.use(express.json());
 
 // Define a route to handle student data insertion
 app.post('/addstudent', (req, res) => {
   const newStudent = req.body;
 
+  // Log the received student data
   console.log('Received new student data:', newStudent);
+
+  // Validate the data and collect validation errors
+  const validationErrors = validateStudentData(newStudent);
+
+  if (validationErrors.length > 0) {
+    console.error('Invalid student data:', newStudent);
+    console.error('Validation errors:', validationErrors);
+    res.status(400).json({ errors: validationErrors });
+    return;
+  }
 
   // Read the existing students from the Students.json file
   const studentsFilePath = path.join(__dirname, './data/Students.json');
@@ -26,8 +35,10 @@ app.post('/addstudent', (req, res) => {
 
     const students = JSON.parse(data);
 
-    // Add the new student to the list
-    students.students.push(newStudent);
+    // Add the new student to the list if data is valid
+    if (newStudent) {
+      students.students.push(newStudent);
+    }
 
     // Write the updated student data back to the file
     fs.writeFile(studentsFilePath, JSON.stringify(students, null, 2), (err) => {
@@ -51,3 +62,33 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+// Function to validate student data and return validation errors
+function validateStudentData(studentData) {
+  const errors = [];
+
+  if (typeof studentData.first_name !== 'string') {
+    errors.push('First name must be a string');
+  }
+
+  if (typeof studentData.last_name !== 'string') {
+    errors.push('Last name must be a string');
+  }
+
+  if (!/^[a-zA-Z]*$/.test(studentData.first_name)) {
+    errors.push('First name can only contain letters');
+  }
+
+  if (!/^[a-zA-Z]*$/.test(studentData.last_name)) {
+    errors.push('Last name can only contain letters');
+  }
+
+  if ( typeof studentData.current_grade < 1){
+    errors.push('Must be above 1');
+  }
+  
+  if ( typeof studentData.current_grade > 12){
+    errors.push('Must be below 12');
+  }
+  return errors;
+}
